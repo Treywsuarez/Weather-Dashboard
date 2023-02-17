40557f7283654854458f7c8f25cb280977efefab
Treysuarez <treyxsuarez@gmail.com>
2023-02-17 16:08:01 +0000

    Added DOM selectors to js file

---
+++ starter/weather.js
@@ -0,0 +1,15 @@
+// DOM Selectors to access variables from HTML elements
+// Search Inputs 
+var citySearchInput = $("#city-search");
+var citySearchBtn = $("#city-search-button");
+var searchHistoryBtn = $("#clear-history");
+var searchHistoryList = $("#search-history-list");
+
+// Current Weather 
+var currentCity = $("#current-city");
+var currentTemperature = $("#current-temperature");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+var weatherData = $("#weather-data");
+
+// Open Weather APIKey
